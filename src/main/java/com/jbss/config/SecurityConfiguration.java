e1d160280d2d3c8eefc6108b4f6ec94bb7891c04
Leonardo Narciso <lvale@aubay.pt>
2021-08-16 12:23:18 +0100

    Added web security configuration.

---
+++ src/main/java/com/jbss/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.jbss.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user")
+		.password("password")
+		.roles("USER")
+		.and()
+		.withUser("foo")
+		.password("bar")
+		.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
