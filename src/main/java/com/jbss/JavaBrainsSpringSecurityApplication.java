44b6f6467a0f07a9d68f73ed3cf4f8344d5ea08d
Leonardo Narciso <lvale@aubay.pt>
2021-08-16 11:56:23 +0100

    First commit.

---
+++ src/main/java/com/jbss/JavaBrainsSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jbss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsSpringSecurityApplication.class, args);
+	}
+
+}
